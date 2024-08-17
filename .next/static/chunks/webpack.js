a1b17d3faf0f87f248ba7e89884a576c5dc32b94
evan-thoms <evanthoms@outlook.com>
2024-08-17 13:37:21 -0700

    installed lilconfig

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ae68daee5f70582f"; }
+/******/ 		__webpack_require__.h = function() { return "f94da4e1d743e868"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
