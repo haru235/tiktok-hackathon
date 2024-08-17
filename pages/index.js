bdb647b0e9e37c2cf2365c0cc1a4eb333efb46c0
Hala Benchiheb <76851669+HalaBench@users.noreply.github.com>
2024-08-17 18:02:21 +0000

    configure next js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to TikTok Hackathon Project</h1>
+        <p>This is the homepage.</p>
+      </div>
+    );
+  }
+  
