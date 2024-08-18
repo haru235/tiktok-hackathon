ef965766f01372250bad408386077cccb55d1e02
Awab Masroor <53250798+secretstf@users.noreply.github.com>
2024-08-17 21:31:27 -0400

    added rudimentary styling for login

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { firestore } from "@/firebase";
+import { useRouter } from "next/router";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 import {
   collection,
@@ -11,9 +13,10 @@ import {
   setDoc,
 } from "firebase/firestore";
 
+import { Button, Box, Stack, TextField, Fab, Typography } from "@mui/material";
+
 function LoginPage() {
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+  const router = useRouter();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -63,36 +66,123 @@ function LoginPage() {
     setShowPassword(false);
   };
 
+  const handleBack = () => {
+    router.push("/");
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-      <form onSubmit={handleSubmit}>
-        <div style={{ display: "grid", gridTemplateRows: "auto" }}>
-          <input
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        direction="column"
+        width={"400px"}
+        height={"600px"}
+        border={"1px solid gray"}
+        boxShadow={"rgba(0, 0, 0, 0.35) 0px 5px 15px;"}
+        borderRadius={"15px"}
+        display={"flex"}
+        p={1}
+        spacing={2}
+      >
+        <Fab
+          size="small"
+          aria-label="add"
+          sx={{
+            width: "fit-content",
+            backgroundColor: "background.default",
+            outline: "none",
+            boxShadow: "none",
+            justifyContent: "flex-start",
+          }}
+          variant="extended"
+          onClick={handleBack}
+        >
+          <ArrowBackIcon /> &nbsp; Back &nbsp;
+        </Fab>
+
+        <Stack
+          spacing={2}
+          alignContent={"center"}
+          justifyContent={"center"}
+          display={"flex"}
+          p={2}
+        >
+          <TextField
             type="email"
-            placeholder="Email"
+            label="email"
             value={email}
             onChange={handleEmailChange}
+            InputProps = {{
+              sx:{
+              borderRadius: "20px",
+            }}}
           />
-          <input
+          <TextField
             type={showPassword ? "text" : "password"}
-            placeholder="Password"
+            label="password"
             value={password}
             onChange={handlePasswordChange}
+            InputProps = {{
+              sx:{
+              borderRadius: "20px",
+            }}}
           />
-          <label>
-            <input
-              type="checkbox"
-              checked={showPassword}
-              onChange={handleShowPasswordChange}
-            />
-            Show Password
-          </label>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+          <Fab
+            size="small"
+            aria-label="add"
+            sx={{
+              width: "100%",
+              backgroundColor: "background.default",
+              outline: "none",
+              boxShadow: "none",
+              justifyContent: "center",
+            }}
+            variant="extended"
+            onClick={handleBack}
+          >
+            <Typography variant="p" color={"blue"} textTransform={'lowercase'}>
+              Login
+            </Typography>
+          </Fab>
+        </Stack>
+      </Stack>
+    </Box>
   );
+  // return (
+  //   <div>
+  //     <h1>Login Page</h1>
+  //     <form onSubmit={handleSubmit}>
+  //       <div style={{ display: "grid", gridTemplateRows: "auto" }}>
+  //         <input
+  //           type="email"
+  //           placeholder="Email"
+  //           value={email}
+  //           onChange={handleEmailChange}
+  //         />
+  //         <input
+  //           type={showPassword ? "text" : "password"}
+  //           placeholder="Password"
+  //           value={password}
+  //           onChange={handlePasswordChange}
+  //         />
+  //         <label>
+  //           <input
+  //             type="checkbox"
+  //             checked={showPassword}
+  //             onChange={handleShowPasswordChange}
+  //           />
+  //           Show Password
+  //         </label>
+  //         <button type="submit">Submit</button>
+  //       </div>
+  //     </form>
+  //   </div>
+  // );
 }
 
 export default LoginPage;
